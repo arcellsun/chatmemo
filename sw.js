557269c4e1685bb03209f99ea73c3149546f4f46
arcellsun <a01038844609@gmail.com>
2026-04-06 16:33:04 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'chatmemo-v1';
+const urlsToCache = [
+  '/chatmemo/',
+  '/chatmemo/index.html',
+  '/chatmemo/manifest.json',
+  '/chatmemo/icon-192.png',
+  '/chatmemo/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
