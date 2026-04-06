fdb1dfb145ea5e3b37a607b0cb6cfa7b92955c3f
arcellsun <a01038844609@gmail.com>
2026-04-06 19:09:01 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,6 @@
-const CACHE_NAME = 'chatmemo-v1';
-const urlsToCache = [
-  '/chatmemo/',
-  '/chatmemo/index.html',
-  '/chatmemo/manifest.json',
-  '/chatmemo/icon-192.png',
-  '/chatmemo/icon.svg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+// v3
+const CACHE_NAME = 'chatmemo-v3';
+const urlsToCache = ['/chatmemo/','/chatmemo/index.html','/chatmemo/manifest.json','/chatmemo/icon-192.png','/chatmemo/icon-512.png','/chatmemo/icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
